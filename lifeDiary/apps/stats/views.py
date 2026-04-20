c72ddf0b7eb9cb05f8bedc7c0be623783a7304ee
Yeongrok Song <song7868@gmail.com>
2026-04-21 02:26:56 +0900

    refactor(stats): ai_feedback → life_feedback 명칭 변경
      템플릿, 모듈 파일명, import 경로 일괄 변경.

--- lifeDiary/apps/stats/views.py
+++ lifeDiary/apps/stats/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from apps.core.utils import safe_date_parse
-from .feedback import generate_feedback
+from .life_feedback import generate_feedback
 from .use_cases import GetStatsContextUseCase
 
 _get_stats_context = GetStatsContextUseCase()
