b68bf05a3274b6af3072dc79878c03cf4e0517c0
Yeongrok Song <song7868@gmail.com>
2026-05-20 00:44:19 +0900

    fix: smtp test

--- lifeDiary/lifeDiary/settings/prod.py
+++ lifeDiary/lifeDiary/settings/prod.py
@@ -53,9 +53,9 @@ CACHES = {
 # Email (production: Gmail SMTP)
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = os.getenv("EMAIL_PORT", "")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
-EMAIL_USE_TLS = os.getenv("EMAIL_USER_TLS", "")
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
